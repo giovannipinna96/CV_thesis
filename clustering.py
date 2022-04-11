834429705216ccce6f136c18e38431e2a29b8b0a
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-04-11 12:33:00 +0200

    add fuzzy clustering

--- clustering.py
+++ clustering.py
@@ -2,6 +2,7 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from fcmeans import FCM
 
 def get_clusters(X, n_clusters=16, get_centers=False):
     y_pred = create_cluster(X, n_clusters, get_centers)
@@ -18,3 +19,11 @@ def create_cluster( X, n_clusters, get_centers):
         centers_cluster = km.cluster_centers_
 
     return y_predicted, centers_cluster, all_distances
+
+    def create_cluster_fuzzy( X_train, X_test, n_clusters):
+        fcm = FCM(n_clusters=n_clusters)
+        fcm.fit(X_train)
+        y_hard = fcm.predict(X_test)
+        y_soft = fcm.soft_predict(X_test)
+
+    return y_hard, y_soft
