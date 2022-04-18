7f1fab0ea7a36a459218c5c48b8295fab1437c35
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-04-18 15:44:37 +0200

    add part for save leables of the feautre extracted

--- featureExtraction.py
+++ featureExtraction.py
@@ -12,12 +12,14 @@ def extrating_features(model, data, return_nodes: list):
         for X, y in data:  
             if i == 0:
                 all_data = X
+                all_labels = y
                 i = 1
             else:
-                all_data = torch.concat((all_data, X), dim=0)
+                all_data = torch.concat((all_data, X), dim=0) 
+                all_labels = torch.concat((all_labels, y), dim=0) 
         out = feat_ext(all_data)
     features_map = []
     for _, layer in enumerate(return_nodes):
         # qui ci sono anche i 3 canali, noi mettiamo tutto insieme
         features_map.append(out[layer].numpy().reshape(out[layer].shape[0], -1))
-    return features_map
+    return features_map, all_labels
