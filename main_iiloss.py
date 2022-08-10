0b642edfdaec05e1990ca3bde564ce8cbad41c59
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-08-10 14:32:05 +0200

    correction to ce_loss

--- main_iiloss.py
+++ main_iiloss.py
@@ -145,7 +145,7 @@ def train_epoch_iiloss(
         # 4. execute the backward pass given the current loss
         ii_loss.backward(retain_graph = True)
         # 5. calculate the iiloss on the current mini-batch
-        if step % 2 :
+        if not (step % 2):
             ce_loss = loss_fn(out_y, y)
         #ce_loss = loss_fn(out_y, y)
         # 6. execute the backward pass given the current loss
