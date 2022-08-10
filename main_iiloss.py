1db3a9fc9aed51750839ed140387dbd18718d980
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-08-10 14:19:46 +0200

    add alternate

--- main_iiloss.py
+++ main_iiloss.py
@@ -145,9 +145,9 @@ def train_epoch_iiloss(
         # 4. execute the backward pass given the current loss
         ii_loss.backward(retain_graph = True)
         # 5. calculate the iiloss on the current mini-batch
-        #if step % 2 :
-        #    ce_loss = loss_fn(out_y, y)
-        ce_loss = loss_fn(out_y, y)
+        if step % 2 :
+            ce_loss = loss_fn(out_y, y)
+        #ce_loss = loss_fn(out_y, y)
         # 6. execute the backward pass given the current loss
         ce_loss.backward()
         # 7. update the value of the params
