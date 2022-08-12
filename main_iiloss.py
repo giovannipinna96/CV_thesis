ac3b754d8643c41438ef0adec01daaee855e8885
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-08-12 16:42:01 +0200

    alternate

--- main_iiloss.py
+++ main_iiloss.py
@@ -146,14 +146,14 @@ def train_epoch_iiloss(
         # 3. calculate the iiloss on the current mini-batch
         ii_loss = compute_ii_loss(out_z, y, num_classes) 
         # 4. execute the backward pass given the current loss
-        #if not (step % 2):
-        ii_loss.backward(retain_graph = True)
+        if not (step % 2):
+            ii_loss.backward(retain_graph = True)
         # 5. calculate the iiloss on the current mini-batch
-        if not (step % 2) :
-            ce_loss = loss_fn(out_y, y)
-        #ce_loss = loss_fn(out_y, y)
+        #if step % 2 :
+        #    ce_loss = loss_fn(out_y, y)
+        ce_loss = loss_fn(out_y, y)
         # 6. execute the backward pass given the current loss
-            ce_loss.backward()
+        ce_loss.backward()
         # 7. update the value of the params
         optimizer.step()
         if lr_scheduler is not None:
