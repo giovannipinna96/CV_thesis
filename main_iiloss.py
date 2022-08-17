3859613ecdf62952a9da6d43c8edd94ff63b94d0
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-08-17 18:09:26 +0200

    delta

--- main_iiloss.py
+++ main_iiloss.py
@@ -209,7 +209,7 @@ def compute_threshold(model, dataloder, num_classes, device):
 def compute_ii_loss(out_z, labels, num_classes):
     n_datapoints = len(out_z)
     device = out_z.device
-    delta = float("inf")
+    delta = 0.5
     intra_spread = torch.Tensor([0]).to(device)
     inter_separation = torch.Tensor([float("inf")]).to(device)
     class_mean = bucket_mean(out_z, labels, num_classes)
